334511497f088a7771800e62864e9bc5bb42f127
Nolan-30 <adewalenolan@gmail.com>
2026-02-25 13:39:09 +0100

    Test Fini

--- web/themes/custom/mon_theme/js/menu.js
+++ web/themes/custom/mon_theme/js/menu.js
@@ -1,12 +1,13 @@
 (function ($, Drupal) {
   Drupal.behaviors.monThemeMenu = {
     attach: function (context, settings) {
-      // On utilise une méthode plus simple sans le .once()
-      $('.nav-link-toggle', context).off('click').on('click', function (e) {
-        e.preventDefault();
-        console.log("Clic détecté !");
-        $(this).parent().toggleClass('is-open');
-      });
-    }
+      $(".nav-link-toggle", context)
+        .off("click")
+        .on("click", function (e) {
+          e.preventDefault();
+          console.log("Clic détecté !");
+          $(this).parent().toggleClass("is-open");
+        });
+    },
   };
 })(jQuery, Drupal);
